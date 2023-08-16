415c321cd1440a3cc22bb8b6894c56177a6e843e
uniqquej <cjy4477@gmail.com>
2023-08-16 14:27:13 +0900

    feat: 로그인 체크

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+    CheckAuth: async function(req, res, next) {
+        if(req.isAuthenticated()) { // 로그인 여부 반환
+            next(); //다음 미들웨어에게 실행권을 넘겨주는 역할
+        }
+        else {
+            res.redirect('/user/signin');
+        }
+    },
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express     = require('express');
 const router      = express.Router();
+const auth        = require('./auth');
 
-router.get('/', function(req, res) {
+//auth.checkauth 미들웨어 먼저 실행 후 로그인 되어 있으면 다음 미들웨어 실행
+// 로그인이 안 되어있다면 index.html rendering 못함
+router.get('/', auth.CheckAuth, function(req, res) {
     res.render('index');
 });
 
