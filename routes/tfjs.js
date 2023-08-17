4ceed50c63c710bb7a2da55c5769073e0c2bcddb
uniqquej <cjy4477@gmail.com>
2023-08-17 14:14:24 +0900

    feat: regression2

--- routes/tfjs.js
+++ routes/tfjs.js
@@ -1,18 +1,25 @@
 const express     = require('express');
 const router      = express.Router();
 
+const tf = require('@tensorflow/tfjs');
+
 router.get('/regression1', function(req, res) {
     res.render('tfjs/regression1'); // 브라우저에서 학습하고 예측, 서버 할 일X
 });
-
-router.post('/regression2', async function(req, res) {
-        //모델 로드
-        //예측
-    res.send('예측결과');
-}); //클라이언트에서 데이터 받고
-
-router.get('/regression2', function(req, res) {
-    res.render('tfjs/regression2'); // 서버에서 예측 실행
+router.get('/regression2', async function(req, res) {
+    res.render('tfjs/regression2');
 });
+router.post('/regression2', async function(req, res) {
+    let model = await tf.loadLayersModel('https://raw.githubusercontent.com/oralcoder/2023_PASS/main/reg_model.json');
+    model.summary();
+    const data = req.body.data;
+    let testX = data.split(',').map(function(x) {
+        return parseInt(x);
+    });
+    testX = tf.tensor(testX, [testX.length]);
+    const preds = model.predict(testX).arraySync();
+    res.send(preds);
+});
+
 
 module.exports = router;
