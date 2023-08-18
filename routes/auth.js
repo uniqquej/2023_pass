8295bbbe1118a9396e6193fdaf7700b15433f9b4
noodlej <jmh010112@naver.com>
2023-08-18 15:41:31 +0900

    modified on a branch

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ module.exports = {
     CheckAuth: async function(req, res, next) {
         console.log('auth');
         if(req.isAuthenticated()) { // 로그인 여부 반환
+            console.log('로그인 된 사용자입니다.');
             next(); //다음 미들웨어에게 실행권을 넘겨주는 역할
         }
         else {
