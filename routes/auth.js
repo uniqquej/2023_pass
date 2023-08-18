6861b0ec8b896660f1c455dbb4552fae2c0c9d6a
noodlej <jmh010112@naver.com>
2023-08-18 14:49:43 +0900

    modified by noodlej

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 module.exports = {
     CheckAuth: async function(req, res, next) {
+        console.log('auth');
         if(req.isAuthenticated()) { // 로그인 여부 반환
             next(); //다음 미들웨어에게 실행권을 넘겨주는 역할
         }
